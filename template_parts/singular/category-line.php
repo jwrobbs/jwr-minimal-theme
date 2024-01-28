8da56a93fc9feb79e471d39ccfe53ad48efe12ac
Josh Robbs <josh@joshrobbs.com>
2024-01-28 13:09:41 -0500

    Remove parentheiss from output

--- template_parts/singular/category-line.php
+++ template_parts/singular/category-line.php
@@ -8,6 +8,6 @@
  */
 
 $post_categories = get_the_category_list( ', ' );
-$post_categories = "<span class='post-categories'>($post_categories)</span>";
+$post_categories = "<span class='post-categories'>$post_categories</span>";
 
 echo wp_kses_post( $post_categories );
