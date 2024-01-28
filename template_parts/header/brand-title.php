e0f7ae9db9486c54c325946ed8bd8dabb232f65a
Josh Robbs <josh@joshrobbs.com>
2024-01-28 10:14:01 -0500

    Add initial brand/title area

---
+++ template_parts/header/brand-title.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Brand/title area of theme header
+ *
+ * @package JWR_Minimal
+ * @since 2024-01-28
+ * @version 1.0
+ */
+
+if ( is_front_page() ) {
+	$brand_title_element = 'h1';
+} else {
+	$brand_title_element = 'div';
+}
+?>
+<div class='site-title-container'>
+	<<?php echo wp_kses_post( $brand_title_element ); ?> class="site-title">
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+			Josh Robbs
+		</a>
+	</<?php echo wp_kses_post( $brand_title_element ); ?>>
+</div><!-- .site-title-container -->
