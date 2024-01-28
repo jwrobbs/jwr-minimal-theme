e67f058e394137c734a8f3960b98099c56f01710
Josh Robbs <josh@joshrobbs.com>
2024-01-28 12:59:44 -0500

    Add structure and pagination

--- template_parts/archive/the-content.php
+++ template_parts/archive/the-content.php
@@ -7,12 +7,22 @@
  * @version 1.0
  */
 
+echo '<main>';
+
 if ( have_posts() ) {
+	echo '<div class=posts>';
 	while ( have_posts() ) {
 		the_post();
 		include 'post-card.php';
 	}
+	echo '</div><!-- .posts -->';
 } else {
 	// If no content, include the "No posts found" template.
 	require_once 'no-posts.php';
 }
+
+if ( function_exists( 'wp_paginate' ) ) {
+	wp_paginate();
+}
+
+echo '</main>';
