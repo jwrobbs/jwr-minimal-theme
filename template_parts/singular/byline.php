e2743bb8e7d7de1a3c14c8d7e7708e2e5c650200
Josh Robbs <josh@joshrobbs.com>
2024-01-28 10:42:06 -0500

    Add placeholder post byline and content

---
+++ template_parts/singular/byline.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Post byline
+ *
+ * @package JWR_Minimal
+ * @since 2024-01-28
+ * @version 1.0
+ */
+
+echo 'byline';
---
+++ template_parts/singular/the-content.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Post/page content
+ *
+ * @package JWR_Minimal
+ * @since 2024-01-28
+ * @version 1.0
+ */
+
+echo 'the_content';
