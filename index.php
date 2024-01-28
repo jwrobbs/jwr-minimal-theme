c0bc41d465d968bd7dd63d2994f28007bd4ab2ce
Josh Robbs <josh@joshrobbs.com>
2024-01-28 13:09:06 -0500

    Add category to post output

--- index.php
+++ index.php
@@ -12,10 +12,13 @@ get_header();
 // Title.
 require_once 'template_parts/shared/page-title.php';
 
+// Category and Byline for posts.
 if ( is_single() ) {
-	require_once 'template_parts/singular/byline.php';
+	include 'template_parts/singular/category-line.php';
+	include 'template_parts/singular/byline.php';
 }
 
+// Main content.
 if ( is_singular() ) {
 	require_once 'template_parts/singular/the-content.php';
 } elseif ( is_archive() ) {
---
+++ template_parts/singular/category-line.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Category line
+ *
+ * @package JWR_Minimal
+ * @since 2024-01-28
+ * @version 1.0
+ */
+
+$post_categories = get_the_category_list( ', ' );
+$post_categories = "<span class='post-categories'>($post_categories)</span>";
+
+echo wp_kses_post( $post_categories );
