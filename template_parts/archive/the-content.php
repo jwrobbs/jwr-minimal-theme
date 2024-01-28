cb6d6b50606720a4ea34f6e2a49e7994bead158c
Josh Robbs <josh@joshrobbs.com>
2024-01-28 11:31:08 -0500

    Replace placeholder content w template parts

--- template_parts/archive/the-content.php
+++ template_parts/archive/the-content.php
@@ -7,4 +7,12 @@
  * @version 1.0
  */
 
-echo 'the_content';
+if ( have_posts() ) {
+	while ( have_posts() ) {
+		the_post();
+		include 'post-card.php';
+	}
+} else {
+	// If no content, include the "No posts found" template.
+	require_once 'no-posts.php';
+}
