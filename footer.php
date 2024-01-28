344b73c16b731ae690c7471262e4890ed5acf169
Josh Robbs <josh@joshrobbs.com>
2024-01-28 09:48:21 -0500

    Base footer

---
+++ footer.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Based on twentyseventeen theme footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package JWR_Minimal
+ * @since 2024-01-28
+ * @version 1.0
+ */
+
+?>
+
+		</div><!-- #content -->
+
+		<footer id="colophon" class="site-footer">
+			<div class="wrap">
+
+footer
+			</div><!-- .wrap -->
+		</footer><!-- #colophon -->
+	</div><!-- .site-content-contain -->
+</div><!-- #page -->
+<?php wp_footer(); ?>
+
+</body>
+</html>
