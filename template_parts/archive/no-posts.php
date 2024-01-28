c89cd8b9a480b4a37d615895a4b0c0b3009dd298
Josh Robbs <josh@joshrobbs.com>
2024-01-28 11:31:21 -0500

    Add no posts template part

---
+++ template_parts/archive/no-posts.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template part for displaying a message that posts cannot be found.
+ *
+ * @package JWR_Minimal
+ * @since 2024-01-28
+ * @version 1.0
+ */
+
+?>
+<div class="alert alert-warning no-posts">
+	<p>Well aren't you curious! There are no posts to display.</p>
+	<p>Maybe you should explore with the search bar or the menu above.</p>
+	<php get_search_form(); ?>
+</div><!-- .alert alert-warning no-posts -->
