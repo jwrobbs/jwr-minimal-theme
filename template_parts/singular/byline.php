74f4e3534c414ef1e4d5b586b08b62241db33440
Josh Robbs <josh@joshrobbs.com>
2024-01-28 11:05:55 -0500

    Add basic byline

--- template_parts/singular/byline.php
+++ template_parts/singular/byline.php
@@ -7,4 +7,16 @@
  * @version 1.0
  */
 
-echo 'byline';
+$post_date       = get_the_date();
+$author_id       = get_the_author_meta( 'ID', $post->post_author );
+$post_author     = get_the_author_meta( 'display_name', $author_id );
+$post_author_url = get_author_posts_url( $author_id );
+
+$output = <<<HTML
+	<div class="byline"> Posted on 
+		<time class="byline__time" datetime="$post_date">$post_date</time>
+		<span class="byline__author">by <a href="$post_author_url">$post_author</a></span>
+	</div>
+HTML;
+
+echo wp_kses_post( $output );
