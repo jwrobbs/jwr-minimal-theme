ba11109a3b8535b82c027febadb15ca8741c6ab3
Josh Robbs <josh@joshrobbs.com>
2024-01-28 20:04:50 -0500

    Swap spans with divs

--- template_parts/singular/category-line.php
+++ template_parts/singular/category-line.php
@@ -8,6 +8,6 @@
  */
 
 $post_categories = get_the_category_list( ', ' );
-$post_categories = "<span class='post-categories'>$post_categories</span>";
+$post_categories = "<div class='post-categories'>$post_categories</div>";
 
 echo wp_kses_post( $post_categories );
