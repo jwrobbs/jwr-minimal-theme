74832016302ec81f03951656557cafa553a9118b
Josh Robbs <josh@joshrobbs.com>
2024-02-19 02:42:00 -0500

    Work together v1

---
+++ template_parts/singular/pages/work-together.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Content for the Work Together page
+ *
+ * @since 2024-02-19
+ * @package JWR_Minimal
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
+<div class='ready-to-work-together'>
+	<h2>Ready to work together?</h2>
+	<p>The next step is to schedule a call. The button will take you to a form where you can pick a time that works for you.</p>
+	<p><a href='https://book.joshrobbs.com/lite/15-min-free' class='schedule-button'>Schedule Your FREE Call</a></p>
+
+</div>
+<div class='wants-more-info'>
+	<h2>Want more information?</h2>
+	<p>Or maybe you just want to say "Hi!" Either way, I'd love to hear from you. Fill out the form below and I'll get back to you as soon as I can.
+	</p>
+	<?php echo do_shortcode( '[piotnetforms id=2090]' ); ?>
+</div>
