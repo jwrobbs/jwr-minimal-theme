7a2ff888ba71aa5074cd56fe3dabd0e9ef34067a
Josh Robbs <josh@joshrobbs.com>
2024-03-28 16:27:33 -0400

    Code cleanup, minor adjustment to copy

--- template_parts/header/brand-title.php
+++ template_parts/header/brand-title.php
@@ -19,5 +19,5 @@ if ( is_front_page() ) {
 			Josh Robbs
 		</a>
 	</<?php echo wp_kses_post( $brand_title_element ); ?>>
-	<div class='site-description'>Consulting & Development</div><!-- .site-description -->
+	<div class='site-description'>Consulting & Web Development</div><!-- .site-description -->
 </div><!-- .site-title-container -->
